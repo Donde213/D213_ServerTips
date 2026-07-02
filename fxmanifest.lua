44ba6a855b781d00372a930cc915e107409e01f1
D213SCRIPTS <malonedonde@gmail.com>
2026-07-02 03:39:01 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+Config = {}
+
+-- Notify system: 'ox' or 'vms'
+Config.Notify = 'vms'
+
+-- How often tips appear, in minutes
+Config.IntervalMinutes = 2
+
+-- Notification settings
+Config.Title = 'Server Tip'
+Config.Duration = 7000 -- milliseconds
+
+Config.Tips = {
+    'Use /help if you need assistance.',
+    'Remember to follow the server rules.',
+    'You can join our Discord for updates and support.',
+    'Store your vehicle properly to avoid losing it.',
+    'Use /report to contact staff if there is an issue.',
+    'Drive careful. Your car can get destroyed',
+    'Use /seathelp if you cant get your car. even with seatbelt off',
+
+}
